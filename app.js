a607030fcab6e3e3d946fe05e02a363bf8296329
10a7 <10a7@users.noreply.github.com>
2018-01-08 23:18:15 -0800

    Begin basic IP rate limiting

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const fs              = require('fs');
 const cluster         = require('cluster');
+const redis           = require('redis');
 
 if (cluster.isMaster) {
     let processes = require('os').cpus().length;
@@ -20,6 +21,8 @@ if (cluster.isMaster) {
     const path            = require('path');
     const app             = express();
     const helmet          = require('helmet');
+    const limiter         = require('express-rate-limit');
+    const limiterRedisStore = require('rate-limit-redis');
 
     // preload INI into RAM on bootstrap/fork, everything needs it
     // and we want to avoid synchronous processes in the request pipeline
@@ -29,6 +32,64 @@ if (cluster.isMaster) {
     app.set('port', process.env.PORT || 8080);
     app.use(helmet());
     app.use(bodyParser.json());
+
+    // initialize rate limiting
+    // we need redis to rate limit across processes.
+    // attempt to talk to redis first, before initializing.
+    // otherwise we will fall back to per-process rate limiting
+    let redisClient = null;
+    let redisDisabled = true;
+    let APIRateLimiter = null;
+    try {
+        let redisProtocol = settings.getSetting('redis', 'protocol');
+        if (settings.getSetting('redis', 'protocol') === 'redis') {
+            let connString = 'redis://';
+            try {
+                let password = settings.getSetting('password');
+                connString += ':' + password + '@';
+            }
+            catch(e) {
+                console.warn("Redis password not set. Not authenticating.");
+            }
+            connString += settings.getSetting('redis', 'hostname');
+            try {
+                let port = settings.getSetting('redis', 'port');
+                connString += ':' + port;
+            }
+            catch(e) {
+                console.warn("Redis port not set. Defaulting to 6379");
+                connString += ':6379/';
+            }
+            console.warn("Redis at " + connString);
+            redisClient = redis.createClient(connString);
+        }
+        else if (redisProtocol === 'unix') {
+            // TODO: finish socket
+            redisClient = redis.createClient({
+                path: settings.getSetting('redis', 'path')
+            });
+        } else {
+            throw new Error("Bad Redis protocol.");
+        }
+        APIRateLimiter = new limiter({
+            store: new limiterRedisStore({
+                client: redisClient
+            }),
+            max: 120,
+            windowMs: 60000,
+            delayMs: 0
+        });
+    }
+    catch (e) {
+        console.warn("Bad Redis config or Redis not available. Disabling Redis-backed rate limiting.");
+        console.warn(e);
+        APIRateLimiter = new limiter({
+            max: 120,
+            windowMs: 60000,
+            delayMs: 0
+        });
+    }
+    app.use(APIRateLimiter);
 
     // v0/legacy routes
     // XXX abstract
