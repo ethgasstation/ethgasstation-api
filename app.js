3203dfc5aec4164667a9fba54f0157eb7263a141
10a7 <10a7@users.noreply.github.com>
2018-01-10 20:33:59 -0800

    abstract limiter to factory module

--- app.js
+++ app.js
@@ -21,80 +21,22 @@ if (cluster.isMaster) {
     const path            = require('path');
     const app             = express();
     const helmet          = require('helmet');
-    const limiter         = require('express-rate-limit');
-    const limiterRedisStore = require('rate-limit-redis');
 
     // preload INI into RAM on bootstrap/fork, everything needs it
     // and we want to avoid synchronous processes in the request pipeline
     const settings        = require('./lib/EGSSettings');
+    const limiter         = require('./lib/RateLimiter.js');
     settings.loadSettings();
 
     app.set('port', process.env.PORT || 8080);
     app.use(helmet());
     app.use(bodyParser.json());
 
-    // initialize rate limiting
-    // we need redis to rate limit across processes.
-    // attempt to talk to redis first, before initializing.
-    // otherwise we will fall back to per-process rate limiting
-    let redisClient = null;
-    let redisDisabled = true;
-    let APIRateLimiter = null;
-    let limiterOptions = {};
-    try {
-        let redisProtocol = settings.getSetting('redis', 'protocol');
-        if (settings.getSetting('redis', 'protocol') === 'redis') {
-            // XXX abstract to helper
-            let connString = 'redis://';
-            try {
-                let password = settings.getSetting('password');
-                connString += ':' + password + '@';
-            }
-            catch(e) {
-                console.warn("Redis password not set. Not authenticating.");
-            }
-            connString += settings.getSetting('redis', 'hostname');
-            try {
-                let port = settings.getSetting('redis', 'port');
-                connString += ':' + port;
-            }
-            catch(e) {
-                console.warn("Redis port not set. Defaulting to 6379");
-                connString += ':6379/';
-            }
-            console.warn("Redis at " + connString);
-            redisClient = redis.createClient(connString);
-        }
-        else if (redisProtocol === 'unix') {
-            // TODO: finish socket
-            redisClient = redis.createClient({
-                path: settings.getSetting('redis', 'path')
-            });
-        } else {
-            throw new Error("Bad Redis protocol.");
-        }
-        limiterOptions = {
-            store: new limiterRedisStore({
-                client: redisClient
-            }),
-            max: 120,
-            windowMs: 60000,
-            delayMs: 0
-        };
-    }
-    catch (e) {
-        console.warn("Bad Redis config or Redis not available. Disabling Redis-backed rate limiting.");
-        limiterOptions = {
-            max: 120,
-            windowMs: 60000,
-            delayMs: 0
-        };
-    }
-    // add benchmarking exception
+    // configure rate limiting
     if (process.env.NO_RATE_LIMIT) {
         console.warn("EXPLICIT SECURITY BYPASS: Rate limiting security disabled");
     } else {
-        app.use(new limiter(limiterOptions));
+        app.use(limiter);
     }
 
     // v0/legacy routes
@@ -110,7 +52,6 @@ if (cluster.isMaster) {
     });
 
     app.get('/', (req, res) => {
-        console.log('hit heartbeat');
         res.json({
             result: 'success'
         });
